671e975e56b573324cf1d02df2c9789b57f2207d
samuel <zhangjun03402@163.com>
2019-12-07 20:17:39 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=zhangjun03402
 
-export MYCLUSTER=
+export MYCLUSTER=tektonknativebeijing57
 export KUBECONFIG=
 
-export NAMESPACE=
-export EMAIL=
-export INGRESS=
+export NAMESPACE=zjtest
+export EMAIL=zhangjun03402@163.com
+export INGRESS=tektonknativebeijing57-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
